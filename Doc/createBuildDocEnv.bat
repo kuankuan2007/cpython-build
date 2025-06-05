50b1ca863838a246f4ce4c1fefc33c620b754edf
kuankuan2007 <2163826131@qq.com>
2025-06-06 03:57:03 +0800

    build-3.10.18-2025-06-06

--- Doc/createBuildDocEnv.bat
+++ Doc/createBuildDocEnv.bat
@@ -1,7 +1,7 @@
 
 set PYTHON=J:\cpython\venv\Scripts\python.exe
 
-set SPHINXBUILD=E:\cpython\venv\Scripts\sphinx-build.exe
+set SPHINXBUILD=J:\cpython\venv\Scripts\sphinx-build.exe
 set SCRIPT_DIR=%~dp0
 set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
 
