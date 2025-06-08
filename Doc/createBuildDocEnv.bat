78f8305ff4bfce1565047b4eb68705c4392fc162
kuankuan2007 <2163826131@qq.com>
2025-06-08 23:58:32 +0800

    build-3.6.15-2025-06-08

---
+++ Doc/createBuildDocEnv.bat
@@ -0,0 +1,15 @@
+
+set PYTHON=J:\cpython\venv\Scripts\python.exe
+
+set SPHINXBUILD=J:\cpython\venv\Scripts\sphinx-build.exe
+set MSBUILD=c:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe
+set SCRIPT_DIR=%~dp0
+set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+
+set NOW_WORKING_DOC_DIR=%~dp0
+set NOW_WORKING_DOC_DIR=%NOW_WORKING_DOC_DIR:~0,-1%
+for %%i in ("%NOW_WORKING_DOC_DIR%") do set NOW_WORKING_DIR=%%~dpi
+
+
+set HTMLHELP=%NOW_WORKING_DIR%externals\windows-installer\htmlhelp\hhc.exe
+set PATH=%NOW_WORKING_DIR%externals\windows-installer\htmlhelp;%PATH%
